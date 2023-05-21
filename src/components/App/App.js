6719962c8801cf0ed6a85e12e10e6f58f195b8f8
Ruslan Bestaev <ruslanbestaev77@yandex.ru>
2023-05-22 00:51:36 +0300

    оформить header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import './App.css';
+import Header from "../Header/header";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header/header.js
@@ -0,0 +1,15 @@
+import "./header.css"
+
+function Header () {
+
+return(
+<header className="header">
+<h1 className="header__title">
+Search for books  
+</h1>
+</header>
+);
+
+
+}
+export default Header;
